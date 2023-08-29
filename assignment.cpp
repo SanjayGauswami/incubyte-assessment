72631cd92c75d222ad15a52345041976e68a8dbe
SanjayGauswami <139198052+SanjayGauswami@users.noreply.github.com>
2023-08-29 22:10:02 +0530

    Update assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -14,8 +14,7 @@ void move_forward(Spacecraft& spacecraft) {
     else if (spacecraft.direction == "S") spacecraft.y--;
     else if (spacecraft.direction == "E") spacecraft.x++;
     else if (spacecraft.direction == "W") spacecraft.x--;
-    else if (spacecraft.direction == "Up") spacecraft.z++;
-    else if (spacecraft.direction == "Down") spacecraft.z--;
+   
 }
 
 void move_backward(Spacecraft& spacecraft) {
@@ -23,31 +22,15 @@ void move_backward(Spacecraft& spacecraft) {
     else if (spacecraft.direction == "S") spacecraft.y++;
     else if (spacecraft.direction == "E") spacecraft.x--;
     else if (spacecraft.direction == "W") spacecraft.x++;
-    else if (spacecraft.direction == "Up") spacecraft.z--;
-    else if (spacecraft.direction == "Down") spacecraft.z++;
+  
 }
 
-void turn_left(Spacecraft& spacecraft) {
-    std::vector<std::string> directions = {"N", "W", "S", "E", "Up", "Down"};
-    int currentIndex = std::find(directions.begin(), directions.end(), spacecraft.direction) - directions.begin();
-    int newIndex = (currentIndex + 1) % directions.size();
-    spacecraft.direction = directions[newIndex];
-}
 
-void turn_right(Spacecraft& spacecraft) {
-    std::vector<std::string> directions = {"N", "E", "S", "W", "Up", "Down"};
-    int currentIndex = std::find(directions.begin(), directions.end(), spacecraft.direction) - directions.begin();
-    int newIndex = (currentIndex + 1) % directions.size();
-    spacecraft.direction = directions[newIndex];
-}
 
-void turn_up(Spacecraft& spacecraft) {
-    if (spacecraft.direction != "Up") spacecraft.direction = "Up";
-}
 
-void turn_down(Spacecraft& spacecraft) {
-    if (spacecraft.direction != "Down") spacecraft.direction = "Down";
-}
+
+
+
 
 void execute_commands(Spacecraft& spacecraft, const std::vector<char>& commands) {
     for (char command : commands) {
@@ -58,18 +41,7 @@ void execute_commands(Spacecraft& spacecraft, const std::vector<char>& commands)
             case 'b':
                 move_backward(spacecraft);
                 break;
-            case 'l':
-                turn_left(spacecraft);
-                break;
-            case 'r':
-                turn_right(spacecraft);
-                break;
-            case 'u':
-                turn_up(spacecraft);
-                break;
-            case 'd':
-                turn_down(spacecraft);
-                break;
+            
             default:
                 break;
         }
